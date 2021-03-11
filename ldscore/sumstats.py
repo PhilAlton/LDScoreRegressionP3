f2fc4d84b34a52aa3667e332a79b5515d16ebce3
PhilAlton <phil.alton@gmail.com>
2021-03-11 19:30:07 +0000

    Update sumstats.py

--- ldscore/sumstats.py
+++ ldscore/sumstats.py
@@ -529,7 +529,7 @@ def _rg(sumstats, args, log, M_annot, ref_ld_cnames, w_ld_cname, i):
         n_snp = np.sum(ii)  # lambdas are late binding, so this works
         sumstats = sumstats[ii]
     n_blocks = min(args.n_blocks, n_snp)
-    ref_ld = sumstats.as_matrix(columns=ref_ld_cnames)
+    ref_ld = sumstats.values(columns=ref_ld_cnames)
     intercepts = [args.intercept_h2[0], args.intercept_h2[
         i + 1], args.intercept_gencov[i + 1]]
     rghat = reg.RG(s(sumstats.Z1), s(sumstats.Z2),
